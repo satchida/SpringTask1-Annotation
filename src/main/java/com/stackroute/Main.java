13269d46f2f42937cffbe382be9ad9aeca8000a3
satchida <you@example.com>
2019-07-15 01:26:40 +0530

    First Commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
-        Movie movie =context.getBean(Movie.class);
+        Movie movie =context.getBean("movie",Movie.class);
         System.out.println(movie);
 
     }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,16 +1,27 @@
 package com.stackroute.domain;
 
 
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     private Actor actor1;
     private Actor actor2;
     private Actor actor3;
 
+      public Movie() {
+
+
+    }
     public Movie(Actor actor1, Actor actor2, Actor actor3) {
         this.actor1 = actor1;
         this.actor2 = actor2;
@@ -18,9 +29,21 @@ public class Movie {
     }
 
     @Autowired
-    public Movie() {
 
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(s);
     }
 
     @Override
